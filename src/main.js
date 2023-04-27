829fe54c78b5db0257c722df8c617becf243a650
LariVelace <131285148+LariVelace@users.noreply.github.com>
2023-04-27 11:27:18 -0300

    alterações java

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { example } from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+document.getElementById("submit").addEventListener("click",()=>{
+    const pokemon = document.getElementById("pokemon").value
+    console.log(pokemon)
+})
 console.log(example, data);
+

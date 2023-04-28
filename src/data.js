a9d8b3d4a1fc56f7ef478e8938ea3b4a7fa495e0
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-04-28 15:23:19 -0300

    função de exibir a imagem do pokemon

--- src/data.js
+++ src/data.js
@@ -1,17 +1,25 @@
 
-export const pokemon = (namePokemon, data) => {
+export const pokemonImage = (namePokemon, data) => {
 
-  for(let array in data){
-    for(let object in data[array]){
-      if(data[array][object].name === namePokemon){
-        console.log(data[array][object])
-
+  for (let array in data) {
+    for (let object in data[array]) {
+      if (data[array][object].name === namePokemon) {
+        /*
+        const linkImage = data[array][object].img;
+        console.log(data[array][object].img);
+        console.log(`link da imagem: ${linkImage}`);
+        return linkImage;
+        */
+        console.log(data[array][object].img);
+        const linkImage = data[array][object].img;
+        return linkImage;
 
       }
 
     }//for
 
   }//for
+
   //console.log(data.pokemon[0].name)
 
 }//endPokemon
--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import { pokemon } from './data.js';
+import { pokemonImage } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 document.getElementById("submit").addEventListener("click", ()=>{
   const namePokemon = document.getElementById("namePokemon").value;
-  pokemon(namePokemon, data);
-
+  const urlImage = pokemonImage(namePokemon, data);
+  changeImage( urlImage );
 })
+
+function changeImage( url ) {
+  console.log(`Url ${url}`);
+  console.log(typeof(url));
+  const image = document.getElementById("imagePokemon");
+  image.src = url.toString();
+}
+
+
 
 
 

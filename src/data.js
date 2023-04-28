bb90e461bb7475873451c115352d929e68750c11
Larissa de Paula <larissadepaulagoncalves@gmail.com>
2023-04-28 13:16:07 -0300

    dataJS

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
-// estas funciones son de ejemplo
+const dataLol = {
+  // filtra os campeões de acordo com uma tag específica
+  buscarTag: function(campeoes, tag) {
+    return campeoes.filter(campeao => campeao.tags.includes(tag));
+  },
+  // filtra os campeões de acordo com um nome específico
+  buscarNome: function(campeoes, nome) {
+    const filtraCampeoes = campeao => campeao.name.includes(nome);
+    return campeoes.filter(filtraCampeoes);
+  }
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+}
+
+export default dataLol

8e98210ca5b828f15fd0648235ced2a58f5cea1d
LariVelace <131285148+LariVelace@users.noreply.github.com>
2023-05-01 15:41:20 -0300

    alterações em js

--- src/data.js
+++ src/data.js
@@ -1,60 +1,29 @@
-export const pokemon = (namePokemon,data) => {
+const pokemonObject = {
+  "name": "",
+  "photo": "",
+  "about": "",
+  "type": "",
+  "evolution": ""
+
+}
+export const pokemon = (namePokemon, data) => {
+  console.log("entrei na função")
   for (let array in data) {
+    console.log("entrei no primeiro for")
     for (let object in data[array]) {
       if (data[array][object].name === namePokemon) {
-       const pokemon{
-        img:const image = data[array][object].img;
-       }
-
+        pokemonObject.name = data[array][object].name;
+        pokemonObject.photo = data[array][object].img;
+        pokemonObject.about = data[array][object].about;
+        pokemonObject.type = data[array][object].type;
+        return pokemonObject
       }
-     
+
+      
+
+
     }//for
 
   }//for
 }
-/*export const pokemonImage = (namePokemon, data) => {
-
-  for (let array in data) {
-    for (let object in data[array]) {
-      if (data[array][object].name === namePokemon) {
-        /*
-        const linkImage = data[array][object].img;
-        console.log(data[array][object].img);
-        console.log(`link da imagem: ${linkImage}`);
-        return linkImage;
-        */
-       /* console.log(data[array][object].img);
-        const linkImage = data[array][object].img;
-        return linkImage;
-
-      }
-     
-    }//for
-
-  }//for
-
-  //console.log(data.pokemon[0].name)
-
-}//endPokemon
-
-
-/*export const pokemonName = (namePokemon, data) => {
-
-  for (let array in data) {
-    for (let object in data[array]) {
-      if (data[array][object].name === namePokemon) {
-      const name = data[array][object].name;
-        return name;
-
-      }
-     
-    }//for
-
-  }//for
-
-  //console.log(data.pokemon[0].name)
-
-}//endPokemon
-
-*/
 
--- src/main.js
+++ src/main.js
@@ -1,25 +1,30 @@
-import { pokemonImage, pokemonName } from './data.js';
+import { pokemon } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 document.getElementById("submit").addEventListener("click", ()=>{
   const namePokemon = document.getElementById("namePokemon").value;
-  const urlImage = pokemonImage(namePokemon, data);
+  /*const urlImage = pokemonImage(namePokemon, data);
   changeImage( urlImage );
   const name = pokemonName(namePokemon,data)
-  changeName(name)
+  changeName(name)*/
+  const pokemonInfo = pokemon (namePokemon,data)
+  console.log ("Pokemon info abaixo")
+  console.log (pokemonInfo)
+  insertToTag ( "name",pokemonInfo.name)
+  changeImage ("imagePokemon",pokemonInfo.photo)
+  insertToTag ("about",pokemonInfo.about)
+  insertToTag ("type",pokemonInfo.type)
 })
 
-function changeImage( url ) {
-  console.log(`Url ${url}`);
-  console.log(typeof(url));
-  const image = document.getElementById("imagePokemon");
+function changeImage( id,url ) {
+  const image = document.getElementById(id);
   image.src = url.toString();
 }
 
-function changeName( name ) {
-  document.getElementById("name").innerHTML=name;
+function insertToTag ( id,txt ) {
+  document.getElementById( id ).innerHTML=txt.toString();
 }
 
 

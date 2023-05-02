86f734b786136814c9d9c4a816ec0165127268f7
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-05-01 21:17:22 -0300

    ajustes

--- src/data.js
+++ src/data.js
@@ -4,23 +4,15 @@ export const pokemonImage = (namePokemon, data) => {
   for (let array in data) {
     for (let object in data[array]) {
       if (data[array][object].name === namePokemon) {
-        /*
-        const linkImage = data[array][object].img;
-        console.log(data[array][object].img);
-        console.log(`link da imagem: ${linkImage}`);
-        return linkImage;
-        */
         console.log(data[array][object].img);
         const linkImage = data[array][object].img;
         return linkImage;
 
-      }
+      }//endIf
 
-    }//for
+    }//endFor
 
-  }//for
-
-  //console.log(data.pokemon[0].name)
+  }//endFor
 
 }//endPokemon
 

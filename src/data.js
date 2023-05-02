6fdcac591c5b5abecacd8ffbc8bb1543b17ff12e
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-05-01 22:44:49 -0300

    buscando a evolução do pokemon

--- src/data.js
+++ src/data.js
@@ -4,22 +4,26 @@ const pokemonObject = {
   "about": "",
   "type": "",
   "evolution": ""
+};
 
-}
 export const pokemon = (namePokemon, data) => {
-  console.log("entrei na função")
-  for (let array in data) {
-    console.log("entrei no primeiro for")
-    for (let object in data[array]) {
-      if (data[array][object].name === namePokemon) {
-        pokemonObject.name = data[array][object].name;
-        pokemonObject.photo = data[array][object].img;
-        pokemonObject.about = data[array][object].about;
-        pokemonObject.type = data[array][object].type;
+  for (let allObjects in data) {
+    for (let object in allObjects) {
+      if (data[allObjects][object].name === namePokemon) {
+        pokemonObject.name = data[allObjects][object].name;
+        pokemonObject.photo = data[allObjects][object].img;
+        pokemonObject.about = data[allObjects][object].about;
+        pokemonObject.type = data[allObjects][object].type;
+        if(data[allObjects][object].evolution['next-evolution'][0].name !== ''){
+          pokemonObject.evolution = data[allObjects][object].evolution['next-evolution'][0].name;
+          if(data[allObjects][object].evolution['next-evolution'][0]['next-evolution'].name !== ''){
+            pokemonObject.evolution = data[allObjects][object].evolution['next-evolution'][0]['next-evolution'][0].name;
+          }
+        }
         return pokemonObject
       }
 
-      
+
 
 
     }//for

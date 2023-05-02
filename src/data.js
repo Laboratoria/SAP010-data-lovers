b46391d6a707710b2c07c9508b5b2df90eeed2d9
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-05-02 12:22:22 -0300

    restaurando o css anterior

--- src/data.js
+++ src/data.js
@@ -8,18 +8,29 @@ const pokemonObject = {
 
 export const filterData = (namePokemon, data) => {
   for (let allObjects in data) {
-    for (let object in allObjects) {
+    console.log("primeiro for")
+    for (let object in data[allObjects]) {
+      console.log("segundo for")
+      console.log(namePokemon)
+      console.log(data[allObjects][object].name)
+      let isTrue = data[allObjects][object].name === namePokemon;
+      console.log(isTrue)
       if (data[allObjects][object].name === namePokemon) {
         pokemonObject.name = data[allObjects][object].name;
         pokemonObject.photo = data[allObjects][object].img;
         pokemonObject.about = data[allObjects][object].about;
         pokemonObject.type = data[allObjects][object].type;
+         /*
         if(data[allObjects][object].evolution['next-evolution'][0].name !== ''){
           pokemonObject.evolution = data[allObjects][object].evolution['next-evolution'][0].name;
+
           if(data[allObjects][object].evolution['next-evolution'][0]['next-evolution'].name !== ''){
             pokemonObject.evolution = data[allObjects][object].evolution['next-evolution'][0]['next-evolution'][0].name;
           }
+
         }
+        */
+        console.log(pokemonObject)
         return pokemonObject
       }
 

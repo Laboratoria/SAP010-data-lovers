1cda1b714cd283b7a0b9389207fd54a8a1dfe67d
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-05-05 08:49:45 -0300

    projeto

--- src/data.js
+++ src/data.js
@@ -1,9 +1,21 @@
 // estas funciones son de ejemplo
+ export const pokemonArquivo = () => {
 
-export const example = () => {
-  return 'example';
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import  pokemonArquivo from "./data/pokemon/pokemon.json";
+// import data from './data/lol/lol.js';  
+pokemonArquivo().then(data => { // operação assíncrona /"pokemonArquivo()"= busca o arquivo/ "the(data=>)" pega o result. e executa o codigo
+    console.log(data); // Imprime o objeto com os dados dos Pokémons no console
 
-console.log(example, data);
+})
+
+
+
+import  pokemonArquivo2 from "./data/pokemon/pokemon.js";
+// import data from './data/lol/lol.js';  
+pokemonArquivo2().then(data => { // operação assíncrona /"pokemonArquivo()"= busca o arquivo/ "the(data=>)" pega o result. e executa o codigo
+    console.log(data); // Imprime o objeto com os dados dos Pokémons no console
+
+})
+
+
+

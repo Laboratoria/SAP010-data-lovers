e5192b197f1e548b81010034baa522a0ead9b1c2
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-05-05 11:38:25 -0300

    main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import  pokemonArquivo from "./data/pokemon/pokemon.json";
-// import data from './data/lol/lol.js';  
+import pokemonArquivo from "./data/pokemon/pokemon.json";
+
 pokemonArquivo().then(data => { // operação assíncrona /"pokemonArquivo()"= busca o arquivo/ "the(data=>)" pega o result. e executa o codigo
     console.log(data); // Imprime o objeto com os dados dos Pokémons no console
 

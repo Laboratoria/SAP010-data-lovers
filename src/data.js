7565faebd7e65654891347ed585a27991407bf96
maahnbraz <marinanbraz@gmail.com>
2023-05-10 16:49:53 -0300

    Estrutura basica

--- src/data.js
+++ src/data.js
@@ -1,16 +1,60 @@
- /*estas funciones son de ejemplo
+const dataFunctions = {
+  filtrar: function (characters, value, key) {
+    const filtro = characters.filter(function (personagem) {
+      const filtrado = personagem[key].toLowerCase() === value;
+      return filtrado;
+    });
 
-export const example = () => {
-  return 'example';
+    return filtro;
+  },
+
+  ordenarCrescente: function (characters) {
+    const ordem = characters.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return ordem;
+  },
+
+  ordenarDecrescente: function (characters) {
+    const ordem = characters.sort(function (a, b) {
+      if (a.name > b.name) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+
+    return ordem;
+  },
+
+  ordenar: function (characters, sortBy) {
+    const copy = [...characters];
+
+    if (sortBy === "ascending") {
+      return dataFunctions.ordenarCrescente(copy);
+    } else if (sortBy === "descending") {
+      return dataFunctions.ordenarDecrescente(copy);
+    }
+  },
+
+  buscarPorNome: function (characters, name) {
+    const filtro = characters.filter(function (personagem) {
+      const buscaFiltrada = personagem.name
+        .toLowerCase()
+        .includes(name.toLowerCase());
+      return buscaFiltrada;
+    });
+    return filtro;
+  },
+
+  calcularPorcentagem: function (tamanhoList, tamanhoListaFiltrada) {
+    return Math.round((tamanhoListaFiltrada * 100) / tamanhoList);
+  },
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};*/
-sortData(data, sortBy, sortOrder){
-
-}
-
-computeStats(data){
-  
-}
+export default dataFunctions;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,69 @@
 import data from "./data/breakingbad/breakingbad.js";
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import dataFunctions from "./data.js";
+
+const characters = data.breaking_bad;
+const cardContainer = document.querySelector("#card-container");
+const selectStatus = document.querySelector("#select-status");
+const percentage = document.querySelector("#percentage");
+const searchForName = document.querySelector("#btn-search");
+const reset = document.querySelector("#reset");
+
+function displayCards(characters) {
+  const arrayResults = characters.map((item) => {
+    const template = `
+        <div class="card">
+
+            <img class="img" src="${item.image}" alt="${item.name}">
+
+            <ul class="card-text" style="list-style: none">                       
+            <li>Name: ${item.name}</li>
+            <li>NickName: ${item.nickname}</li>
+            <li>Status: ${item.status}</li>
+            <li>Occupation: ${item.occupation}</li>
+            <li>Birthday: ${item.birthday}</li>
+            <li>Portrayed: ${item.portrayed}</li>
+            <li>Category: ${item.category}</li>
+           
+            </ul>                
+
+        </div>
+        `;
+    return template;
+  });
+  return arrayResults.join("");
+}
+cardContainer.innerHTML = displayCards(characters);
+
+selectStatus.addEventListener("change", (event) => {
+  const value = event.target.value;
+  const listaFiltrada = dataFunctions.filtrar(characters, value, "status");
+  const cards = displayCards(listaFiltrada);
+  cardContainer.innerHTML = cards;
+
+  const porcentagem = dataFunctions.calcularPorcentagem(
+    characters.length,
+    listaFiltrada.length
+  );
+  percentage.innerHTML =
+    "Essa categoria contém " + porcentagem + "% dos personagens totais";
+});
+
+selectOrder.addEventListener("change", (event) => {
+  const value = event.target.value;
+  const listaOrdenada = dataFunctions.ordenar(characters, value);
+  const cards = displayCards(listaOrdenada);
+  cardContainer.innerHTML = cards;
+  percentage.innerHTML = "";
+});
+
+reset.addEventListener("click", (event) => {
+  location.reload(event);
+});
+
+searchForName.addEventListener("keyup", function (event) {
+  const value = event.target.value;
+  const listaFiltrada = dataFunctions.buscarPorNome(characters, value);
+  const cards = displayCards(listaFiltrada);
+  cardContainer.innerHTML = cards;
+});

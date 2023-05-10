c9fba4ef8fc1ae1de6d454db319530e24af11b54
fernandacaniçais <fernanda.canicais@outlook.com>
2023-05-10 14:01:22 -0300

    resolvendo bug git hub

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import { example } from './data.js';
+import data from './data/atlhetes/athletes.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 const atletas = data.athletes
 
 const esportes = atletas.map(modalidades => modalidades.sport)
-
-
-
 const país = atletas.map(países => países.team)
 
 

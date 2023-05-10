d3bc8833e43787a67dcb89fd0f6da4850aedb406
PamelaBrsa <pamela.barbosa16@yahoo.com.br>
2023-05-10 13:50:51 -0300

    filtros

--- src/data.js
+++ src/data.js
@@ -3,4 +3,36 @@
 import data from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-const paísesFiltrados = document.querySelector(".selecionar-país")
+
+
+
+const atletas = data.athletes
+
+const esportes = atletas.map(modalidades => modalidades.sport)
+
+
+
+const país = atletas.map(países => países.team)
+
+
+const esportesFiltrados = new Set()
+esportes.forEach((esporte) => {
+    esportesFiltrados.add(esporte)
+})
+console.log(esportesFiltrados)
+
+const paísesFiltrados = new Set()
+país.forEach((países) => {
+    paísesFiltrados.add(países)
+})
+
+
+
+
+/*Muitas modalidades para colocar no menu
+
+const modalidade = atletas.map(estilo => estilo.event)
+
+const modalidadesFiltradas = new Set()
+modalidade.forEach((modalidades) => {
+    modalidadesFiltradas.add(modalidades)
--- src/main.js
+++ src/main.js
@@ -1,44 +1,9 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/athletes/athletes.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 60f650b45c9569cec5c7c4f3b5b078bf860468b8
-const atletas = data.athletes
-
-const esportes = atletas.map(modalidades => modalidades.sport)
 
 
-
-const país = atletas.map(países => países.team)
-
-
-const esportesFiltrados = new Set()
-esportes.forEach((esporte) => {
-    esportesFiltrados.add(esporte)
-})
-console.log(esportesFiltrados)
-
-const paísesFiltrados = new Set()
-país.forEach((países) => {
-    paísesFiltrados.add(países)
-})
-
-
-
-
-/*Muitas modalidades para colocar no menu
-
-const modalidade = atletas.map(estilo => estilo.event)
-
-const modalidadesFiltradas = new Set()
-modalidade.forEach((modalidades) => {
-    modalidadesFiltradas.add(modalidades)
-<<<<<<< HEAD
-})*/
+const paísesFiltrados = document.querySelector(".selecionar-país")
 
 
 
@@ -46,7 +11,3 @@ modalidade.forEach((modalidades) => {
 
 
 
-
-=======
-})*/
->>>>>>> 60f650b45c9569cec5c7c4f3b5b078bf860468b8

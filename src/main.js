d57b560010371b3019aaa6e36576107fd3bdbc19
maahnbraz <marinanbraz@gmail.com>
2023-05-11 12:50:02 -0300

    adiciona comentarios e retirar linha

--- src/main.js
+++ src/main.js
@@ -2,16 +2,17 @@ import data from "./data/breakingbad/breakingbad.js";
 
 import dataFunctions from "./data.js";
 
-const characters = data.breaking_bad;
-const cardContainer = document.querySelector("#card-container");
-const selectStatus = document.querySelector("#select-status");
-const percentage = document.querySelector("#percentage");
-const searchForName = document.querySelector("#btn-search");
-const reset = document.querySelector("#reset");
+const characters = data.breaking_bad; //aqui onde puxa o banco de dados do breakingbad.js para fornecer os personagens
+const cardContainer = document.querySelector("#card-container"); //Aqui cria os cards
+const selectStatus = document.querySelector("#select-status"); //Seletor de busca do status
+const searchForName = document.querySelector("#btn-search"); //cria busca por nome
+const reset = document.querySelector("#reset"); //cria o argumento de reset
 
 function displayCards(characters) {
+  //declara a função personagens, criando os cards
   const arrayResults = characters.map((item) => {
-    const template = `
+    //aqui esta a função MAP
+    const template = `                   
         <div class="card">
 
             <img class="poster-img" src="${item.img}" alt="${item.name}">
@@ -28,7 +29,7 @@ function displayCards(characters) {
 
         </div>
         `;
-    return template;
+    return template; //a constante acima cria todo o card e suas caracteristicas, puxando os dados da base de dados
   });
   return arrayResults.join("");
 }

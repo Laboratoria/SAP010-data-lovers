7738673e01befa0ca8c1475e0300dca2067135b4
Jéssica Zanon <jessicatorreszanon@gmail.com>
2023-05-12 12:05:32 -0300

    puxamos as bandeiras dos países com map

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-//import { example } from './data.js';
 import countries from './data/countries/countries.js';
 
-import data from './data/countries/countries.js';
+const exibirFlagsDiv = document.getElementById('exibirFlags');
+console.log(countries)
+const flagsCountries = countries.countries.map(country => {
+    const flagsDiv = document.createElement('table');
+    const nameElement = document.createElement('h2');
+    const flagElement = document.createElement('img');
 
-console.log(data);
+    nameElement.textContent = country.name.common;
+    flagElement.src = country.flags.png;
+    flagElement.textContent = country.flag;
+
+
+    flagsDiv.appendChild(nameElement);
+    flagsDiv.appendChild(flagElement);
+
+    return flagsDiv;
+});
+
+flagsCountries.forEach(flag => {
+    exibirFlagsDiv.appendChild(flag);
+});

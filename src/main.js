77fa0a2a5062a25b7d43c1ede762e3563862a556
Fernanda Araújo de Souza <asouza.fernanda@gmail.com>
2023-05-13 11:26:29 -0300

    inclusão de imagens na página

--- src/main.js
+++ src/main.js
@@ -1,15 +1,39 @@
-import { example } from './data.js'; //importando do data.js
+import got from './data/got/got.js'; // importando o módulo 'got' do arquivo got.js
 
-import data from './data/got/got.js'; // importando do got.js
+const cardContainer = document.querySelector("#exibirCards"); // selecionando o elemento com id 'exibirCards' para exibir os cards
+
+const cardsGot = got.got.map(personagens => {
+    const cardDiv = document.createElement('div');
+    const firstName = document.createElement('h2');
+    const lastName = document.createElement('h2');
+    const imageUrl = document.createElement('img');
+
+    firstName.textContent = personagens.firstName.common;
+    lastName.textContent = personagens.lastName.common;
+    imageUrl.src = personagens.imageUrl; // corrigindo para acessar a propriedade 'imageUrl' de cada personagem
+
+    cardDiv.appendChild(firstName);
+    cardDiv.appendChild(lastName);
+    cardDiv.appendChild(imageUrl);
+
+    return cardDiv;
+});
+
+cardsGot.forEach(card => {
+    cardContainer.appendChild(card);
+});
 
 
-document.getElementById("buscar").addEventListener("click", onClickEnviar); // add o evento de clique ao botão vinculando a função
+
+
+
+/*document.getElementById("buscar").addEventListener("click", onClickEnviar); // add o evento de clique ao botão vinculando a função
 
 function onClickEnviar() {
- console.log(example, data);
 
 
     const ordenar = document.getElementById('ordenar'); //lê o input 'ordenar'
     const familia = document.getElementById('familia'); //lê o input 'familia'
 
-}
+}*/
+    

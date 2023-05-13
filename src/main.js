8c00cb8658c7e7e7371e3d1f559c2db211893612
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-05-13 20:37:09 -0300

    estilizando page3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 //import { example } from './data.js';
+// em example van las funciones que creara Cristyna
 
 
 import data from './data/pokemon/pokemon.js';
 
 
-console.log(example, data);
+const input = document.querySelector("#searchByName") // devuelve el primero Element dentro del documento que coincide con ese id
+input.addEventListener("keyup", e => {  //input: elemento que se escucha, keyup: muestra la info que el usuario escribio, evento: accion realizada 
+    //console.log(input.value)  // value: devuelve el valor en un capo de texto

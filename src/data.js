65332ba81e9d3c1bf73e11caf7899658f3fd73b5
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-05-13 15:38:40 -0300

    adicionando page3.html

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-// estas funciones son de ejemplo
+/* estas funciones son de ejemplo
 
 export const example = () => {
   return 'example';
@@ -7,3 +7,4 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import { example } from './data.js';
+
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 
 console.log(example, data);

0062ace6bcbb7bbeeaec92a58791dd80340befe1
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-05-13 17:13:34 -0300

    segunda pagina

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,19 +1,16 @@
-test('fetchPokemon deve retornar os dados do pokemon correto', async () => {
+test('pokemonName deve retornar os dados do pokemon correto', async () => {
+//(test async () => { ) é a estrutura padrão para formular um test 
 
-
-  const pokemonName = 'pikachu';
-  const pokemonData = await fetchPokemon(pokemonName);
-  expect(pokemonData.name).toEqual('pikachu');
-  expect(pokemonData.id).toEqual(25);
+  const pokemonName = 'pikachu'; // APIresponse busca e retorna as informações solicitadas  "pikachu"
+  const pokemonData = await pokemonName('pikachu');// await busca as informações de forma assincronica 
+  expect(pokemonData.name).toEqual('pikachu'); // verifica se o nome do pokemon retornado na constante pokedata é igual a pikachu / toEqual quer dizer que é igual 
+  expect(pokemonData.id).toEqual(25); //para verificar se o id do Pokémon retornado na constante pokemonData é igual a undefined / toEqual quer dizer que é igual 
 });
 
-test('fetchPokemon deve retornar null quando o pokemon não é encontrado', async () => {
-  const pokemonName = 'naoexiste';
-  const pokemonData = await fetchPokemon(pokemonName);
+test('pokemonName deve retornar Not found quando o pokemon não é encontrado', async () => {
+  const pokemonName = 'not-found-pokemon'; // simula uma resposta da API quando a função é chamada com um Pokémon
+  const pokemonData = await pokemonName(); //é chamada assincronamente 
   expect(pokemonData).toBeNull();
 });
-it('returns `anotherExample`', () => {
-expect(anotherExample()).toBe('OMG');
-});
 
-console.log()
+//console.log()

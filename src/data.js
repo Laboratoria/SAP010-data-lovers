dcb983e2c7cbc6892a46d8de7259020433900820
Diulianne Oliveira <diulicarine@gmail.com>
2023-05-15 14:16:04 -0400

    testando comentarios

--- src/data.js
+++ src/data.js
@@ -7,3 +7,19 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+/*
+O coração deste projeto é a manipulação de dados através de arrays e objetos.
+
+Recomendamos que este arquivo contenha toda a funcionalidade que corresponda a obter, processar e manipular dados (suas funções):
+
+filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+
+sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
+
+computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
+
+Estes nomes de funções e parâmetros são somente referência, o que vocês decidir utilizar vai depender da sua implementação.
+
+Estas funções devem ser puras e independentes do DOM. Estas funções serão depois usadas a partir do arquivo src/main.js, ao carregar a página e a cada vez que o usuário interagir com a interface (cliques, seleções, filtros, ordenação, etc).
+*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
+import { example } from './data.js'; //puxa o filtro do data (nome, temporada em que aparece e etc)
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+/*utilize src/main.js para todos os códigos que tenham a ver com a exibição dos dados na tela. Com isto nos referimos basicamente à interação com o DOM. Operações como criação de nós, registro de manejadores de eventos (event listeners ou event handlers) e etc.
+
+Esta não é a única forma de dividir seu código. Pode utilizar mais arquivos e pastas, sempre e quando a estrutura estiver clara para suas colegas.
+
+Neste arquivo você encontrará uma séris de imports comentados. Para carregar diferentes fontes de dados, você deverá "descomentar" estos imports. Cada um destes imports criará uma variável data com os dados correspondentes à fonte escolhida.
+
+*/

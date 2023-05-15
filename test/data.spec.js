7a93370ef1f9fde4296a058f9f9e2498d1900e05
maahnbraz <marinanbraz@gmail.com>
2023-05-15 15:34:45 -0300

    iniciando os testes

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,42 +1,79 @@
+import { dataFunctions } from "../src/data.js";
 
-import { dataFunctions } from '../src/data.js';
-
-/*const arrayTest = [
+const arrayTest = [
   {
     char_id: 1,
     name: "Walter White",
-    birthday: "09-07-1958",
-    occupation: ["High School Chemistry Teacher", "Meth King Pin"],
-    img: "https://upload.wikimedia.org/wikipedia/en/0/03/Walter_White_S5B.png",
     status: "Presumed dead",
-    nickname: "Heisenberg",
-    appearance: [1, 2, 3, 4, 5],
-    portrayed: "Bryan Cranston",
     category: "Breaking Bad",
-    better_call_saul_appearance: [],
   },
   {
     char_id: 2,
     name: "Jesse Pinkman",
-    birthday: "09-24-1984",
-    occupation: ["Meth Dealer"],
-    img: "https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441",
     status: "Alive",
-    nickname: "Cap n' Cook",
-    appearance: [1, 2, 3, 4, 5],
-    portrayed: "Aaron Paul",
     category: "Breaking Bad",
-    better_call_saul_appearance: [],
   },
-] */
-describe('dataFunctions', () => {
-  it('is a object', () => {
-    expect(typeof dataFunctions).toBe('object');
-  });
-});
+  {
+    char_id: 3,
+    name: "Skyler White",
+    status: "Alive",
+    category: "Breaking Bad",
+  },
+  {
+    char_id: 4,
+    name: "Saul Goodman",
+    status: "Alive",
+    category: "Breaking Bad, Better Call Saul",
+  },
+];
 
-describe('filter', () => {
-  it('is a function', () => {
-    expect(typeof filter).toBe('function');
+describe("dataFunctions", () => {
+  it("is a object", () => {
+    expect(typeof dataFunctions).toBe("object");
+  });
+}),
+  describe("filter", () => {
+    it("is a function", () => {
+      expect(typeof filter).toBe("function");
+    });
+  }),
+  //testando ordenação a-z e z-a
+  describe("order", () => {
+    it("is a function", () => {
+      expect(typeof dataFunctions.order).toBe("function");
+    });
+
+    it("order ascending", () => {
+      const character = dataFunctions.order(arrayTest, "ascending");
+      expect(character.length).toEqual(4);
+      expect(character).toEqual([
+        arrayTest[2],
+        arrayTest[3],
+        arrayTest[4],
+        arrayTest[1],
+      ]);
+    });
+
+    it("order descending", () => {
+      const character = dataFunctions.order(arrayTest, "descending");
+      expect(character.length).toEqual(4);
+      expect(character).toEqual([
+        arrayTest[1],
+        arrayTest[4],
+        arrayTest[3],
+        arrayTest[2],
+      ]);
+    });
+  });
+
+//testando a busca por nome
+describe("searchName", () => {
+  it("search for name", () => {
+    expect(typeof dataFunctions.searchName).toBe("function");
+  });
+
+  it("search for a name", () => {
+    const character = dataFunctions.searchName(arrayTest, "Walter");
+    expect(character.length).toEqual(2);
   });
 });

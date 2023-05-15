836b4357814e5ec13c33742a28cdead2977c1a6c
maahnbraz <marinanbraz@gmail.com>
2023-05-15 11:29:49 -0300

    inicio dos teste

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,7 @@
-import { example, anotherExample } from '../src/data.js';
+import dataFunctions from "../src/data.js";
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe("dataFunctions", () => {
+  it("dataFunctions is a object", () => {
+    expect(typeof dataFunctions).toBe("object");
   });
 });

1ebe1c987b01271a43b77530da5068a94178a8e6
maahnbraz <marinanbraz@gmail.com>
2023-05-15 23:29:07 -0300

    desbugando os testes

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import { dataFunctions } from "../src/data.js";
+import dataFunctions from "../src/data.js";
 
 const arrayTest = [
   {
@@ -31,40 +31,42 @@ describe("dataFunctions", () => {
   it("is a object", () => {
     expect(typeof dataFunctions).toBe("object");
   });
-}),
-  describe("filter", () => {
-    it("is a function", () => {
-      expect(typeof filter).toBe("function");
-    });
-  }),
-  //testando ordenação a-z e z-a
-  describe("order", () => {
-    it("is a function", () => {
-      expect(typeof dataFunctions.order).toBe("function");
-    });
+});
 
-    it("order ascending", () => {
-      const character = dataFunctions.order(arrayTest, "ascending");
-      expect(character.length).toEqual(4);
-      expect(character).toEqual([
-        arrayTest[2],
-        arrayTest[3],
-        arrayTest[4],
-        arrayTest[1],
-      ]);
-    });
-
-    it("order descending", () => {
-      const character = dataFunctions.order(arrayTest, "descending");
-      expect(character.length).toEqual(4);
-      expect(character).toEqual([
-        arrayTest[1],
-        arrayTest[4],
-        arrayTest[3],
-        arrayTest[2],
-      ]);
-    });
+describe("filter", () => {
+  it("is a function", () => {
+    expect(typeof dataFunctions.filter).toBe("function");
   });
+});
+
+//testando ordenação a-z e z-a
+describe("order", () => {
+  it("is a function", () => {
+    expect(typeof dataFunctions.order).toBe("function");
+  });
+
+  it("order ascending", () => {
+    const character = dataFunctions.order(arrayTest, "ascending");
+    expect(character.length).toEqual(4);
+    expect(character).toEqual([
+      arrayTest[1],
+      arrayTest[3],
+      arrayTest[2],
+      arrayTest[0],
+    ]);
+  });
+
+  it("order descending", () => {
+    const character = dataFunctions.order(arrayTest, "descending");
+    expect(character.length).toEqual(4);
+    expect(character).toEqual([
+      arrayTest[0],
+      arrayTest[2],
+      arrayTest[3],
+      arrayTest[1],
+    ]);
+  });
+});
 
 //testando a busca por nome
 describe("searchName", () => {
@@ -73,7 +75,7 @@ describe("searchName", () => {
   });
 
   it("search for a name", () => {
-    const character = dataFunctions.searchName(arrayTest, "Walter");
-    expect(character.length).toEqual(2);
+    const characterSearch = dataFunctions.searchName(arrayTest, "Walter");
+    expect(characterSearch.length).toEqual(1);
   });
 });

45df8c97b73513da5b5d5d507b0919f6568f0273
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-05-16 11:19:02 -0300

    modificado data.js

--- src/data.js
+++ src/data.js
@@ -1,28 +1,2 @@
 export {sortByName,   filterByType,   searchByName, };
 
-const sortByName = (data, valor) => {
-  const newData = [].concat(data);
-  if (valor === "A-Z") {
-    newData.sort((a, b) => a.name.localeCompare(b.name));
-  } else {
-    newData.sort((a, b) => b.name.localeCompare(a.name));
-  }
-  return newData;
-};
-
-
-const filterByType = (data, valor) => {
-  const filterPkmnByType = data.filter(
-    (pokemon) => pokemon.type.indexOf(valor.toLowerCase()) > -1
-  );
-  return filterPkmnByType;
-};
-
-const searchByName = (pokemon, name) => {
-  return pokemon.filter((pokemon) => {
-    return pokemon.name.toLowerCase().includes(name.toLowerCase());
-  });
-};
-
-
-

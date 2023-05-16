852ae8c982eee74f43184cd4f9ad9924546ae7aa
Jéssica Zanon <jessicatorreszanon@gmail.com>
2023-05-16 17:09:30 -0300

    conseguimos fazer o filtro dos continentes funcionarem

--- src/data.js
+++ src/data.js
@@ -1,26 +1,80 @@
-// estas funciones son de ejemplo
+function filtroAz (arr, opcao) {
+  if (opcao == 'az'){
+    return arr.sort((a, b) => {
+      if (a.name.common < b.name.common) {
+          return -1;
+      }
+      if (a.name.common > b.name.common) {
+          return 1;
+      }
+      return 0;
+  })
+}
 
-import countries from "./data/countries/countries";
+else if(opcao == 'za'){
+  return arr.sort((a, b) => {
+    if (a.name.common > b.name.common) {
+      return -1;
+    } 
+    if (a.name.common < b.name.common) {
+      return 1;
+    } 
+      return 0;
+    });
+    } else{arr}
+  }
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  function filtroCont(arr, opcao) {
+    const america = [];
+    const europe = [];
+    const asia = [];
+    const africa = [];
+    const oceania = [];
+    const antarctica = [];
 
-/*function ordemPaisesCrescente(countries) {
-  countries.sort((a,b) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].continents == "America") {
+        america.push(arr[i]);
+      }
+      else if (arr[i].continents == "Europe") {
+        europe.push(arr[i]);
+      }
+      else if (arr[i].continents == "Asia") {
+        asia.push(arr[i]);
+      }else if (arr[i].continents == "Africa") {
+        africa.push(arr[i]);
+      }else if (arr[i].continents == "Oceania") {
+        oceania.push(arr[i]);
+      }
+      else  {
+        antarctica.push(arr[i]);
+      }
+  }
+  if(opcao == 'Oceania'){
+    return oceania
+  }
+  else if (opcao == 'Ásia'){
+    return asia
+  }
+  else if (opcao == 'Europa'){
+    return europe
+  }
+  else if (opcao == 'América'){
+  return america
+}
+else if (opcao == 'África'){
+  return africa
+}
+else if (opcao == 'Antártida'){
+  return antarctica
+}
+else {
+  return arr
+}
+}
+
   
-  if (a.name.common <b.name.common){
-  return -1;
-  }
-  else if(a.name.common > b.name.common) {
-  return 1;
-  }
-  else{
-  
-  }
-});
-  }*/
+
+
+export { filtroAz, filtroCont}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,45 @@
+import {filtroAz, filtroCont } from './data.js';
 import countries from './data/countries/countries.js';
 
-const exibirFlagsDiv = document.getElementById('exibirFlags');
-console.log(countries)
-const flagsCountries = countries.countries.map(country => {
-    const flagsDiv = document.createElement('table');
-    const nameElement = document.createElement('h2');
-    const flagElement = document.createElement('img');
+function exibirPaises() {
+  let filtroOp = document.getElementById('opcao');
+  let exibirFlagsDiv = document.getElementById('exibirFlags');
+  exibirFlagsDiv.innerHTML = '';
 
+  let op = filtroOp.value;
+
+  let flagsCountries = filtroCont(countries.countries, op).map(country => {
+    let flagsDiv = document.createElement('div');
+    let nameElement = document.createElement('h3');
+    let flagElement = document.createElement('img');
+    flagElement.classList.add('imgBand');
     nameElement.textContent = country.name.common;
     flagElement.src = country.flags.png;
-    flagElement.textContent = country.flag;
-
 
     flagsDiv.appendChild(nameElement);
     flagsDiv.appendChild(flagElement);
+    flagsDiv.classList.add('bands');
 
     return flagsDiv;
-});
+  });
 
-flagsCountries.forEach(flag => {
+  flagsCountries.forEach(flag => {
     exibirFlagsDiv.appendChild(flag);
-});
+  });
+}
+
+// Chamar a função ao carregar a página
+window.addEventListener('load', exibirPaises);
+
+// Adicionar o evento de mudança ao select
+let filtroOp = document.getElementById('opcao');
+filtroOp.addEventListener('change', exibirPaises);
+
+
+let elementosDeOrdenacao = document.getElementById('az');
+elementosDeOrdenacao.addEventListener('change', filtroPaises);
+function filtroPaises(){
+let resultado = filtroAz(countries.countries, 'az');
+console.log(resultado)
+}
+

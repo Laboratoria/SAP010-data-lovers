9a4e058288de2b7843900e613cdfc5f18fc4726d
CamilaVerso <camilaverso2@gmail.com>
2023-05-17 13:59:29 -0300

    Primeiro commit teste

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/tarot/tarot.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);

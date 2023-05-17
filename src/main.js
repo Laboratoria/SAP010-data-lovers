88f7be1191d9738426f4b92f781df752f9a23534
mairabergamaschi <mairabergama@gmail.com>
2023-05-16 22:28:26 -0300

    estrutura básica HTML e CSS com menu hamburguer funcinando, sem cards

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.querySelector(".menu-icon")
+    menuIcon.addEventListener("click", switchMenu);
+  
+  
+  function switchMenu() {
+    const menuDisplay = document.querySelector(".menu").style.display
+    if (menuDisplay === "none" || !menuDisplay) {
+      document.querySelector(".menu").style.display = 'flex'
+      document.querySelector(".menu-icon").innerHTML = "<i class='fas fa-times fa-lg'></i>"
+    } else {
+      document.querySelector(".menu").style.display = 'none'
+      document.querySelector(".menu-icon").innerHTML = "<i class='fas fa-bars fa-lg'></i>"
+    }
+  }
 console.log(example, data);
+})

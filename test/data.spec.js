4123dbb82a74bfb7840fe429acf090d3572f013f
Geane Ramos <geaneramos1995@gmail.com>
2023-05-18 11:43:16 -0300

    cobertura total de testes

--- test/data.spec.js
+++ test/data.spec.js
@@ -39,23 +39,24 @@ describe("filter", () => {
   });
 
   it("filter status", () => {
-    const selectStatus = dataFunctions.filter(arrayTest, "Alive", "Status");
+    const selectStatus = dataFunctions.filter(arrayTest, "Alive", "status");
     expect(selectStatus.length).toEqual(3);
-    expect(selectStatus).toEqual(arrayTest[1], arrayTest[2], arrayTest[3]);
+    expect(selectStatus).toEqual([arrayTest[1], arrayTest[2], arrayTest[3]]);
   });
 
   it("filter series", () => {
     const selectCategory = dataFunctions.filter(
       arrayTest,
       "Breaking Bad",
-      "Series"
+      "category"
     );
     expect(selectCategory.length).toEqual(4);
-    expect(selectCategory).toEqual(
+    expect(selectCategory).toEqual([
       arrayTest[0],
       arrayTest[1],
-      arrayTest[(2, arrayTest[3])]
-    );
+      arrayTest[2],
+      arrayTest[3],
+    ]);
   });
 });
 

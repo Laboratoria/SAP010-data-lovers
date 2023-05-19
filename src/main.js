4c957fe3988107db1069c7683812d8b0785572d2
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-05-19 10:16:48 -0300

    estilizando page2

--- src/main.js
+++ src/main.js
@@ -1,48 +1,52 @@
-import { sortByName, sortByNum, filterByType } from "./data.js";
+import { filterByType } from "./data.js";
 import data from "./data/pokemon/pokemon.js";
-
 
 const pkmnDataList = data.pokemon;
 const pkmCards = document.getElementById("pokemonList");
 
-pkmnDataList.forEach((pokemon) => { //forEach: estrutura de repetição, usada para manipular os elementos do array pokemon
+function updatePokemonList(filteredPokemons) {
+  pkmCards.innerHTML = "";
 
-  const card = document.createElement("div"); // createElement foi usado para criar uma div, na qual teremos vários elementos
-  let typePkm = ""; // a variável vazia typePkm é criada, posteriormente armazenará os valores do tipo
-  pokemon.type.forEach((type) => { // dentro da matriz pokemon, estamos procurando e atribuindo a um parágrafo os diferentes tipos
-    typePkm += `<p class="type-style ${type}">${type}</p>`;
-  }); /* innerHTML se uso para definir o novo conteúdo dentro do HTML, ou seja, que tipo, nome, imagem e número 
-       foram adicionados ao HTML via interpolação de string*/
-  card.innerHTML = ` 
-    
-  <div class="pokemon">
-  <p class="pokemon-numero">${pokemon.num}</p>
-    <div class="pokemon-imagem">
-    <img src="${pokemon.img}" alt="Foto pokemon"></img>
-  </div>
-  <div class="name-contenedor">
-        <h2 class="pokemon-name">${pokemon.name}</h2>
-  </div>
-  <div class="pokemon-type">
-  <div class="typePokemon">${typePkm}</div>
-</div>
-      `;
-  pkmCards.appendChild(card); /* O createElement não funciona sozinho, por isso usamos appendChild() para adicionar esses novos valores que estamos criando*/
-})
-//console.log (pkmCards)
+  filteredPokemons.forEach((pokemon) => {
+    const card = document.createElement("div");
+    let typePkm = "";
+    pokemon.type.forEach((type) => {
+      typePkm += `<p class="type-style ${type}">${type}</p>`;
+    });
+    card.innerHTML = `
+      <div class="pokemon">
+        <p class="pokemon-numero">${pokemon.num}</p>
+        <div class="pokemon-imagem">
+          <img src="${pokemon.img}" alt="Foto pokemon"></img>
+        </div>
+        <div class="name-contenedor">
+          <h2 class="pokemon-name">${pokemon.name}</h2>
+        </div>
+        <div class="pokemon-type">
+          <div class="typePokemon">${typePkm}</div>
+        </div>
+      </div>
+    `;
 
-const filterType = document.getElementById("typeFilter");
-let dataFiltered = []
-
-if (filterType) {
-  filterType.addEventListener("change", function () {
-    dataFiltered = pkmnDataList.filter((pokemon) => {    
-      return pokemon.type[0] === filterType.value.toLowerCase()
-    })
-  
-    console.log(dataFiltered)
+    pkmCards.appendChild(card);
   });
 }
+
+const typeInput = document.getElementById("typeFilter");
+typeInput.addEventListener("change", () => {
+  if (typeFilter.value != "All"){
+   const selectedType = typeInput.value.toLowerCase();
+  const filteredPokemons = filterByType(selectedType);
+  updatePokemonList(filteredPokemons);
+     }
+});
+
+// Chamada inicial para exibir todos os cards
+updatePokemonList(pkmnDataList);
+
+
+
+
 
 const searchByNameInput = document.getElementById("searchByName");
 let resultSearch = []

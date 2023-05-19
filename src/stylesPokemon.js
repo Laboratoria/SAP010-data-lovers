dbca0856c641ff5a74a8a5bdf43bd8a647b12930
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-05-19 09:10:26 -0300

    criação de novo objeto para estylização

---
+++ src/stylesPokemon.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    typePokmeon: 'grass',
+    img: './images/types/grass.png',
+    color: '#5fa81f7e'
+  },
+  {
+    typePokmeon: 'poison',
+    img: './images/types/poison.png',
+    color: '#786fa6d0'
+  },
+  {
+    typePokmeon: 'fire',
+    img: './images/types/fire.png',
+    color: '#ff793f7a'
+  },
+  {
+    typePokmeon: 'flying',
+    img: './images/types/flying.png',
+    color: '#778beb'
+  },
+  {
+    typePokmeon: 'water',
+    img: './images/types/water.png',
+    color: '#3dc1d371'
+  },
+  {
+    typePokmeon: 'bug',
+    img: './images/types/bug.png',
+    color: '#0be8816e'
+  },
+  {
+    typePokmeon: 'normal',
+    img: './images/types/normal.png',
+    color: '#aaa69ddc'
+  },
+  {
+    typePokmeon: 'electric',
+    img: './images/types/electric.png',
+    color: '#CCAE62'
+  },
+  {
+    typePokmeon: 'ground',
+    img: './images/types/ground.png',
+    color: '#CCAE62'
+  },
+  {
+    typePokmeon: 'fighting',
+    img: './images/types/fighting.png',
+    color: '#ea8685'
+  },
+  {
+    typePokmeon: 'psychic',
+    img: './images/types/psychic.png',
+    color: '#cf6a87'
+  },
+  {
+    typePokmeon: 'rock',
+    img: './images/types/rock.png',
+    color: '#84817a'
+  },
+  {
+    typePokmeon: 'ice',
+    img: './images/types/ice.png',
+    color: '#63cdda'
+  },
+  {
+    typePokmeon: 'ghost',
+    img: './images/types/ghost.png',
+    color: '#786fa6'
+  },
+  {
+    typePokmeon: 'dragon',
+    img: './images/types/dragon.png',
+    color: '#546de5'
+  },
+  {
+    typePokmeon: 'fairy',
+    img: './images/types/fairy.png',
+    color: '#f8a5c2'
+  },
+  {
+    typePokmeon: 'dark',
+    img: './images/types/dark.png',
+    color: '#353b48'
+  },
+  {
+    typePokmeon: 'steel',
+    img: './images/types/steel.png',
+    color: '#487eb0'
+  },
+]

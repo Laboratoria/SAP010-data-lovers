239aad17af35eb770df46d127a894e06ec6304ac
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-05-19 10:10:42 -0300

    Refatoração da função styleCard

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { computeStats, filterData, sortData } from './data.js';
 import data from './data/pokemon/pokemon.js';
-
+import stylesPokemon from './stylesPokemon.js';
 
 const name = document.getElementById("namePokemon");
 
@@ -42,7 +42,6 @@ if (findPokemonHeight) {
   });
 }//endIf
 
-
 const findPercentageOfTypesOfPokemons = document.getElementById("findPercentageOfTypesOfPokemons");
 
 if (findPercentageOfTypesOfPokemons) {
@@ -51,103 +50,35 @@ if (findPercentageOfTypesOfPokemons) {
   });
 }//endIf
 
-const styleCard = (pokemon) => {
+const styleCard = (pokemon, stylesPokemon) => {
 
   for (const data in pokemon) {
 
-    Object.defineProperty(pokemon[data], 'imgType', { value: [] })
-    Object.defineProperty(pokemon[data], 'colorType', { value: [] })
+    Object.defineProperty(pokemon[data], 'imgType', { value: [] });
+    Object.defineProperty(pokemon[data], 'colorType', { value: [] });
 
-    for (let i = 0; i < pokemon[data].type.length; i++) {
+    stylesPokemon.forEach(style => {
 
-      if (pokemon[data].type[i].includes('grass')) {
-        pokemon[data].imgType.push("./images/types/grass.png");
-        pokemon[data].colorType.push('#5fa81f7e');
-      }
-      if (pokemon[data].type[i].includes('poison')) {
-        pokemon[data].imgType.push("./images/types/poison.png");
-        pokemon[data].colorType.push('#786fa6d0');
-      }
-      if (pokemon[data].type[i].includes('fire')) {
-        pokemon[data].imgType.push("./images/types/fire.png");
-        pokemon[data].colorType.push('#ff793f7a');
-      }
-      if (pokemon[data].type[i].includes('flying')) {
-        pokemon[data].imgType.push("./images/types/flying.png");
-        pokemon[data].colorType.push('#778beb');
-      }
-      if (pokemon[data].type[i].includes('water')) {
-        pokemon[data].imgType.push("./images/types/water.png");
-        pokemon[data].colorType.push('#03f2faa8');
-      }
-      if (pokemon[data].type[i].includes('bug')) {
-        pokemon[data].imgType.push("./images/types/bug.png");
-        pokemon[data].colorType.push('#0be8816e');
-      }
-      if (pokemon[data].type[i].includes('normal')) {
-        pokemon[data].imgType.push("./images/types/normal.png");
-        pokemon[data].colorType.push('#aaa69ddc');
-      }
-      if (pokemon[data].type[i].includes('electric')) {
-        pokemon[data].imgType.push("./images/types/electric.png");
-        pokemon[data].colorType.push('#fbd1007c');
-      }
-      if (pokemon[data].type[i].includes('ground')) {
-        pokemon[data].imgType.push("./images/types/ground.png");
-        pokemon[data].colorType.push('#cd6133bb');
-      }
-      if (pokemon[data].type[i].includes('fighting')) {
-        pokemon[data].imgType.push("./images/types/fighting.png");
-        pokemon[data].colorType.push('#ea8685');
-      }
-      if (pokemon[data].type[i].includes('psychic')) {
-        pokemon[data].imgType.push("./images/types/psychic.png");
-        pokemon[data].colorType.push('#cf6a87b9');
-      }
-      if (pokemon[data].type[i].includes('rock')) {
-        pokemon[data].imgType.push("./images/types/rock.png");
-        pokemon[data].colorType.push('#84817ab6');
-      }
-      if (pokemon[data].type[i].includes('ice')) {
-        pokemon[data].imgType.push("./images/types/ice.png");
-        pokemon[data].colorType.push('#63ccdab6;');
-      }
-      if (pokemon[data].type[i].includes('ghost')) {
-        pokemon[data].imgType.push("./images/types/ghost.png");
-        pokemon[data].colorType.push('#786fa6c0');
-      }
-      if (pokemon[data].type[i].includes('dragon')) {
-        pokemon[data].imgType.push("./images/types/dragon.png");
-        pokemon[data].colorType.push('#546ce5b0');
-      }
-      if (pokemon[data].type[i].includes('fairy')) {
-        pokemon[data].imgType.push("./images/types/fairy.png");
-        pokemon[data].colorType.push('#f8a5c2e0');
-      }
-      if (pokemon[data].type[i].includes('dark')) {
-        pokemon[data].imgType.push("./images/types/dark.png");
-        pokemon[data].colorType.push('#353b4891');
-      }
-      if (pokemon[data].type[i].includes('steel')) {
-        pokemon[data].imgType.push("./images/types/steel.png");
-        pokemon[data].colorType.push('#487eb0bd');
-      }
+      if (pokemon[data].type.includes(style.typePokemon)) {
 
-    }
+        pokemon[data].imgType.push(style.img);
+        pokemon[data].colorType.push(style.color);
 
-  }
+      }//endIf
 
-}
+    })//endForEach
 
-styleCard(data.pokemon);
+  }//endFor
+
+}//endStyleCard
+
+styleCard(data.pokemon, stylesPokemon);
 
 const buildCard = (id, pokemon) => {
 
   document.getElementById(id).innerHTML = "";
 
-
   const pokemons = document.getElementById(id);
-
 
   for (let i = 0; i < pokemon.length; i++) {
 
@@ -163,16 +94,9 @@ const buildCard = (id, pokemon) => {
     `;
 
     for (let j = 0; j < pokemon[i].imgType.length; j++) {
-      const imgType = document.createElement('img');
-      imgType.classList.add("pokeball");
       pokemonShowName += `
         <img class="pokeball" src=${pokemon[i].imgType[j]}>
     `;
-
-      if (imgType) {
-        imgType.appendChild(cardPokemon);
-
-      }//endIf
     }
 
     pokemonShowName += `
@@ -192,8 +116,6 @@ const buildCard = (id, pokemon) => {
     }//endIf
 
   }//endFor
-
-  console.log(pokemons)
 
 }//endBuildCard
 
--- src/stylesPokemon.js
+++ src/stylesPokemon.js
@@ -1,26 +1,26 @@
 export default [
   {
-    typePokmeon: 'grass',
+    typePokemon: 'grass',
     img: './images/types/grass.png',
     color: '#5fa81f7e'
   },
   {
-    typePokmeon: 'poison',
+    typePokemon: 'poison',
     img: './images/types/poison.png',
     color: '#786fa6d0'
   },
   {
-    typePokmeon: 'fire',
+    typePokemon: 'fire',
     img: './images/types/fire.png',
     color: '#ff793f7a'
   },
   {
-    typePokmeon: 'flying',
+    typePokemon: 'flying',
     img: './images/types/flying.png',
     color: '#778beb'
   },
   {
-    typePokmeon: 'water',
+    typePokemon: 'water',
     img: './images/types/water.png',
     color: '#3dc1d371'
   },
@@ -30,63 +30,63 @@ export default [
     color: '#0be8816e'
   },
   {
-    typePokmeon: 'normal',
+    typePokemon: 'normal',
     img: './images/types/normal.png',
     color: '#aaa69ddc'
   },
   {
-    typePokmeon: 'electric',
+    typePokemon: 'electric',
     img: './images/types/electric.png',
-    color: '#CCAE62'
+    color: '#fbd1007c'
   },
   {
-    typePokmeon: 'ground',
+    typePokemon: 'ground',
     img: './images/types/ground.png',
-    color: '#CCAE62'
+    color: '#cd6133bb'
   },
   {
-    typePokmeon: 'fighting',
+    typePokemon: 'fighting',
     img: './images/types/fighting.png',
     color: '#ea8685'
   },
   {
-    typePokmeon: 'psychic',
+    typePokemon: 'psychic',
     img: './images/types/psychic.png',
-    color: '#cf6a87'
+    color: '#cf6a87b9'
   },
   {
-    typePokmeon: 'rock',
+    typePokemon: 'rock',
     img: './images/types/rock.png',
-    color: '#84817a'
+    color: '#84817ab6'
   },
   {
-    typePokmeon: 'ice',
+    typePokemon: 'ice',
     img: './images/types/ice.png',
-    color: '#63cdda'
+    color: '#63ccdab6'
   },
   {
-    typePokmeon: 'ghost',
+    typePokemon: 'ghost',
     img: './images/types/ghost.png',
-    color: '#786fa6'
+    color: '#786fa6c0'
   },
   {
     typePokmeon: 'dragon',
     img: './images/types/dragon.png',
-    color: '#546de5'
+    color: '#546ce5b0'
   },
   {
-    typePokmeon: 'fairy',
+    typePokemon: 'fairy',
     img: './images/types/fairy.png',
-    color: '#f8a5c2'
+    color: '#f8a5c2e0'
   },
   {
-    typePokmeon: 'dark',
+    typePokemon: 'dark',
     img: './images/types/dark.png',
-    color: '#353b48'
+    color: '#353b4891'
   },
   {
-    typePokmeon: 'steel',
+    typePokemon: 'steel',
     img: './images/types/steel.png',
-    color: '#487eb0'
+    color: '#487eb0bd'
   },
 ]

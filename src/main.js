8cd091fe4431ae7e277ac9d7516964378084610a
Roxane Principe <roxane.principe@gmail.com>
2023-05-19 17:13:09 -0300

    corrigido conflito

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import {
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+let activePokemon = '001';
 const textPokeNum = document.getElementById("text-poke-num")
 const textPokeGenerationName = document.getElementById("text-poke-generation-name")
 const textPokeName = document.getElementById("text-poke-name")
@@ -17,7 +18,7 @@ const textPokeRarity = document.getElementById("text-poke-rarity")
 const imagePoke = document.getElementById("image-poke")
 const textPokeHeight = document.getElementById("text-poke-height")
 const textPokeWeight = document.getElementById("text-poke-weight")
-const textPokeCandy = document.getElementById("text-poke-candy")
+const textPokeSpawnChance = document.getElementById("text-poke-spawn-chance")
 const textPokeAbout = document.getElementById("text-poke-about")
 
 const inputPokeName = document.getElementById("input-poke-name")
@@ -73,13 +74,16 @@ function changeUI(pokemon) {
   imagePoke.alt = pokemon.name
   textPokeHeight.innerHTML = pokemon.size.height
   textPokeWeight.innerHTML = pokemon.size.weight
-  textPokeCandy.innerHTML = "-"
-  if (pokemon.evolution['next-evolution']
-      && pokemon.evolution['next-evolution'].length > 0){
-    textPokeCandy.innerHTML = pokemon.evolution['next-evolution'][0]['candy-cost']
-  }
+  textPokeSpawnChance.innerHTML = pokemon['spawn-chance']
   textPokeAbout.innerHTML = pokemon.about
   textPokeName.innerHTML = firstToUpperCase(pokemon.name)
+
+  //adicionado estas funcoes para dentro do changeUI para quando fazer a pesquisa também mudar a cor da pagina
+  const type = pokemon.type[0];
+  const container = document.querySelector('#container');
+  container.className = '';
+  container.classList.add('container')
+  container.classList.add(type)
 }
 
 function firstToUpperCase(str) {
@@ -89,6 +93,57 @@ function firstToUpperCase(str) {
 
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+  
+// criou uma funcao chamada cardClick que é executada quando clica no card, ela recebe como parametro o elemnento do card 
+const cardClick = (element) => {
+  const value = element.dataset.value; //nesta linha eu pego o numero do pokemon clicado do elemento
+  const pokemon = data.pokemon.find((item) => item.num === value); // nesta linha eu busco nos dados o pokemon que tenha esse numero
+  changeUI(pokemon) //nesta linha eu troco as informacoes da tela
+
+  element.classList.add('active'); // adiciona a classe active no elemento clicado
+  const activeElement = document.querySelector('#pokemon-'+activePokemon) //pega qual o elemento ativo anterior 
+  activeElement.className = 'list-item' //remove todas as classes do elemento menos o item list
+  activePokemon = pokemon.num //troca o pokemon ativo para o elemento clicado
+}
+
+window.cardClick = cardClick;
+
+const renderItem = (props, index) => {
+  const {
+    num,
+    name,
+    img,
+    type: types,
+  } = props;
+
+  return `
+  <div class="list-item ${index===0 ? 'active' : ''}" onclick="cardClick(this)" data-value="${num}" id="pokemon-${num}">
+      <div class="item-picture">  
+        <img src="${img}" alt="${name}">
+      </div>
+      <div class="item-info">
+        <div class="item-title">
+          <p>No. ${num} - ${name}</p>
+        </div>
+        <div class="item-types">
+          ${types.map((type) => (
+            `<div class="item-type ${type}">
+              <p>${type}</p>
+            </div>` 
+          )).join('')} 
+        </div>
+      </div>
+  </div>
+  `;
+}
+
+const initialList = () => {
+  const elements = data.pokemon.map(renderItem).join('\n');
+  const list = document.querySelector('#list-items');
+  list.innerHTML = elements;
+};
+
+window.onload = initialList;
 
 /*const searchButton = document.getElementById("searchButton")
 searchButton.addEventListener("click", function() {

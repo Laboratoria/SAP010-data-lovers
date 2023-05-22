b3a15c1c130cfdd2c48e65dbb6eb01b2017e14dc
Marinacb7 <marinacordeiro92@gmail.com>
2023-05-22 18:01:04 -0300

    Atuaização da página Filmografia e criação de cards filmes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,41 @@
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-import data from './data/ghibli/ghibli.js';
-
-console.log(example, data);
+//import ghibli from './data/ghibli/ghibli.json';
+//import data from './data/ghibli/ghibli.json';
+function createMovieCards(films) {
+    const container = document.getElementById('movie-container');
+  
+    films.forEach((films) => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+  
+      const image = document.createElement('img');
+      image.src = films.poster;
+      image.alt = films.title;
+  
+      const title = document.createElement('title-h3');
+      title.textContent = films.title;
+  
+      const date = document.createElement('release_date');
+      date.textContent = films.release_date;
+  
+      card.appendChild(image);
+      card.appendChild(title);
+      card.appendChild(date);
+      container.appendChild(card);
+    });
+  }
+  // Carregar dados de um arquivo JSON
+  fetch('./data/ghibli/ghibli.json')
+    .then(ghibli => ghibli.json())
+    .then(data => {
+      createMovieCards(data.films);
+    })
+    .catch(error => {
+      console.error('Ocorreu um erro ao carregar os dados:', error);
+    });
+  
+  
+  //console.log(example, data);
+  

1eae81a2edf7417d9ab3715cd9272e62ad8ddaca
Marinacb7 <marinacordeiro92@gmail.com>
2023-05-24 18:46:14 -0300

    Adição das funções getMovies e getDirectors no data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,26 @@
-// estas funciones son de ejemplo
-
-/* export const example = () => {
-  return 'example';
+const data = {
+  getMovies: async function() {
+    try {
+      const response = await fetch('./data/ghibli/ghibli.json');
+      const data = await response.json();
+      return data.films || [];
+    } catch (error) {
+      console.log('Ocorreu um erro ao obter os dados dos filmes:', error);
+      return [];
+    }
+  },
+  
+  getDirectors: async function() {
+    try {
+      const response = await fetch('./data/ghibli/ghibli.json');
+      const data = await response.json();
+      const directors = new Set(data.films.map((film) => film.director));
+      return Array.from(directors);
+    } catch (error) {
+      console.log('Ocorreu um erro ao obter os diretores:', error);
+      return [];
+    }
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-}; */
+export { data };

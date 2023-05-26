1c90bb9adf703ec78b2384aa6752eb5f5af3b02f
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2023-05-25 22:10:26 -0300

    Criamos um novo html(pokestop), um novo css para nosso html, adicionamos comentários em ambos htmls e css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+for (let i = 0; i < data.pokemon.length; i++){
+console.log (data.pokemon[i].name);
+}

94a94407ccf9ae7b7edbaf987ba62f2825c0b229
Vitória Victor <vitoriavictor@Vitorias-MacBook-Pro.local>
2023-05-26 22:38:13 -0300

    css filtros | inclusão de funções de filtros

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 const data = {
-  getMovies: async function() {
+  getMovies: async function () {
     try {
       const response = await fetch('./data/ghibli/ghibli.json');
       const data = await response.json();
@@ -9,8 +9,8 @@ const data = {
       return [];
     }
   },
-  
-  getDirectors: async function() {
+
+  getDirectors: async function () {
     try {
       const response = await fetch('./data/ghibli/ghibli.json');
       const data = await response.json();
@@ -31,83 +31,89 @@ export function convertToArray(obj) {
   return Object.keys(obj).map(key => obj[key]);
 }
 
-  // Função para filtrar os filmes por diretor
-  export function filterByDirector(movies, director) {
-      if (director === "all") {
-        return movies;
-      } else {
-        return movies.filter(movie => movie.director === director);
-      }
-    }
+// Função para filtrar os filmes por diretor
+export function filterByDirector(movies, director) {
+  if (director === "all") {
+    return movies;
+  } else {
+    return movies.filter(movie => movie.director === director);
+  }
+}
 
 
-  // Função para filtrar os personagens por filme
-  export function filterCharactersByMovie(movies, title) {
-    if (title === "all"){
-      const allCharacters = movies.flatMap(movie => movie.people);
-      return allCharacters;
-    }
-    else{
+// Função para filtrar os personagens por filme
+export function filterCharactersByMovie(movies, title) {
+  if (title === "all") {
+    const allCharacters = movies.flatMap(movie => movie.people);
+    return allCharacters;
+  }
+  else {
     return movies.filter(movie => movie.title === title)
-    .map(movie => movie.people)
-    .flat();
-    }
+      .map(movie => movie.people)
+      .flat();
   }
-  
-  // Função para filtrar os personagens por gênero
-  export function filterByGender(movies, gender) {
-    if (gender === "all"){
-      return movies.flatMap(movie => movie.people);
-    } else {
-      const filteredMovies = movies.filter(movie => {
-        return movie.people.some(person => person.gender === gender);
-      });
-  
-      return filteredMovies.flatMap(movie => movie.people);
-    }
-  }
+}
 
-  // Função para ordenar os filmes em ordem alfabética de A-Z
-  export function sortByTitleAZ(movies) {
+// Função para filtrar os personagens por gênero
+export function filterByGender(movies, gender) {
+  if (gender === "all") {
+    return movies.flatMap(movie => movie.people);
+  } else {
+    const filteredMovies = movies.filter(movie => {
+      return movie.people.some(person => person.gender === gender);
+    });
+
+    return filteredMovies.flatMap(movie => movie.people);
+  }
+}
+
+// Função para ordenar os filmes em ordem alfabética de A-Z
+export function sortByTitleAZ(movies) {
   return movies.sort((a, b) => a.title.localeCompare(b.title));
-  }
+}
 
-  // Função para ordenar os filmes em ordem alfabética de Z-A
-  export function sortByTitleZA(movies) {
+// Função para ordenar os filmes em ordem alfabética de Z-A
+export function sortByTitleZA(movies) {
   return movies.sort((a, b) => b.title.localeCompare(a.title));
-  }
- 
-  // Função para ordenar os filmes por ano de lançamento (do mais antigo ao mais recente)
-  export function sortByReleaseYear(movies) {
-  return movies.sort((a, b) => a.release_date - b.release_date);
-  }
+}
 
-  //AINDA NAO
-  
-  // Função para ordenar os filmes por nota do Rotten Tomatoes (do maior ao menor)
-  export function sortByRottenTomatoes(movies) {
+// Função para ordenar os filmes por ano de lançamento (do mais antigo ao mais recente)
+export function sortByReleaseYearOld(movies) {
+  return movies.sort((a, b) => a.release_date - b.release_date);
+}
+// Função para ordenar os filmes por ano de lançamento (mais recente ao mais antigo)
+export function sortByReleaseYearNew(movies) {
+  return movies.sort((a, b) => b.release_date - a.release_date);
+}
+// Função para ordenar os filmes por nota do Rotten Tomatoes (do maior ao menor)
+export function sortByRottenTomatoesHigh(movies) {
   return movies.sort((a, b) => b.rt_score - a.rt_score);
-  }
-  
-  // Função para calcular as estatísticas de gênero dos personagens por filme
-  export function calculateGenderStats(movies, movieTitle) {
+}
+// Função para ordenar os filmes por nota do Rotten Tomatoes (do menor ao maior)
+export function sortByRottenTomatoesLow(movies) {
+  return movies.sort((a, b) => a.rt_score - b.rt_score);
+}
+
+//AINDA NAO
+// Função para calcular as estatísticas de gênero dos personagens por filme
+export function calculateGenderStats(movies, movieTitle) {
   const characters = filterCharactersByMovie(movies, movieTitle);
   const totalCharacters = characters.length;
   const femaleCharacters = characters.filter(character => character.gender === 'Female').length;
   const maleCharacters = characters.filter(character => character.gender === 'Male').length;
   const femalePercentage = (femaleCharacters / totalCharacters) * 100;
   const malePercentage = (maleCharacters / totalCharacters) * 100;
-  
-  return {
-  totalCharacters,
-  femaleCharacters,
-  maleCharacters,
-  femalePercentage,
-  malePercentage
-  };
-  }
 
-  export let movies = [];
+  return {
+    totalCharacters,
+    femaleCharacters,
+    maleCharacters,
+    femalePercentage,
+    malePercentage
+  };
+}
+
+export let movies = [];
 
 fetch('./data/ghibli/ghibli.json')
   .then(response => response.json())
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-  import { data } from "./data.js";
-  import { filterByDirector, filterByGender, filterCharactersByMovie, sortByTitleAZ, sortByTitleZA, sortByReleaseYear, sortByRottenTomatoes, calculateGenderStats } from './data.js';
-  import { movies } from './data.js';
-  
-  // MENU HAMBURGUER
+import { data } from "./data.js";
+import { filterByDirector, filterByGender, filterCharactersByMovie, sortByTitleAZ, sortByTitleZA, sortByReleaseYearOld, sortByReleaseYearNew, sortByRottenTomatoesHigh, sortByRottenTomatoesLow, calculateGenderStats } from './data.js';
+import { movies } from './data.js';
+
+// MENU HAMBURGUER
 
 const btnMobile = document.getElementById('btn-mobile');
 
@@ -31,14 +31,14 @@ async function loadDirectors() {
     while (directorFilter.firstChild) {
       directorFilter.removeChild(directorFilter.firstChild);
     }
-  
+
     // Adicionar opções
     const allOption = document.createElement("option");
     allOption.value = "all";
     allOption.textContent = "Todos";
     directorFilter.appendChild(allOption);
-  
-    directors.forEach(function(director) {
+
+    directors.forEach(function (director) {
       const option = document.createElement("option");
       option.value = director;
       option.textContent = director;
@@ -50,10 +50,10 @@ async function loadDirectors() {
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const directorFilter = document.getElementById('director-filter');
 
-  directorFilter.addEventListener('change', function() {
+  directorFilter.addEventListener('change', function () {
     const selectedDirector = directorFilter.value;
     const filteredMovies = filterByDirector(movies, selectedDirector);
     showMovies(filteredMovies);
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   loadDirectors();
   loadMovies();
-  
+
 
   async function loadMovies() {
     try {
@@ -72,146 +72,159 @@ document.addEventListener('DOMContentLoaded', function() {
       while (movieFilter.firstChild) {
         movieFilter.removeChild(movieFilter.firstChild);
       }
-      
+
       // Adicionar opções
       const allOption = document.createElement("option");
       allOption.value = "all";
       allOption.textContent = "Todos";
       movieFilter.appendChild(allOption);
-    
-      movies.forEach(function(movies) {
+
+      movies.forEach(function (movies) {
         const option = document.createElement("option");
         option.value = movies.title;
         option.textContent = movies.title;
         movieFilter.appendChild(option);
       });
 
-    genderFilter.addEventListener('change', function() {
+      genderFilter.addEventListener('change', function () {
         const selectedGender = genderFilter.value;
         const filteredMovies = filterByGender(movies, selectedGender);
         showCharacter(filteredMovies);
         console.log(filteredMovies);
       });
-    } 
+    }
     catch (error) {
       console.log('Ocorreu um erro ao carregar os personagens:', error);
     }
   }
-  
-     window.addEventListener('DOMContentLoaded', loadMovies); {
-        const movieFilter = document.getElementById('movieFilter');
-      
-        movieFilter.addEventListener('change', function() {
-          const selectedMovie = movieFilter.value;
-          const filteredCharacter = filterCharactersByMovie(movies, selectedMovie);
-          showCharacter(filteredCharacter);
-          console.log(filteredCharacter);
-        })
-      }
-      sortAZButton.addEventListener('click', function() {
-        const sortedAZ = sortAZButton.value;
-        const sortedMovies = sortByTitleAZ(movies, sortedAZ);
-        showMovies(sortedMovies);
-        console.log(sortedMovies);
-    });
-    sortZAButton.addEventListener('click', function() {
-      const sortedZA = sortZAButton.value;
-      const sortedMovies = sortByTitleZA(movies, sortedZA);
-      showMovies(sortedMovies);
-      console.log(sortedMovies);
-  });
-  sortReleaseYearButton.addEventListener('click', function() {
-    const sortedYear = sortReleaseYearButton.value;
-    const sortedMovies = sortByReleaseYear(movies, sortedYear);
+
+  window.addEventListener('DOMContentLoaded', loadMovies); {
+    const movieFilter = document.getElementById('movieFilter');
+
+    movieFilter.addEventListener('change', function () {
+      const selectedMovie = movieFilter.value;
+      const filteredCharacter = filterCharactersByMovie(movies, selectedMovie);
+      showCharacter(filteredCharacter);
+      console.log(filteredCharacter);
+    })
+  }
+  sortAZButton.addEventListener('click', function () {
+    const sortedAZ = sortAZButton.value;
+    const sortedMovies = sortByTitleAZ(movies, sortedAZ);
     showMovies(sortedMovies);
     console.log(sortedMovies);
-});
-sortRottenTomatoesButton.addEventListener('click', function() {
-  const sortedRT = sortRottenTomatoesButton.value;
-  const sortedMovies = sortByRottenTomatoes(movies, sortedRT);
-  showMovies(sortedMovies);
-  console.log(sortedMovies);
-});
-      
+  });
+  sortZAButton.addEventListener('click', function () {
+    const sortedZA = sortZAButton.value;
+    const sortedMovies = sortByTitleZA(movies, sortedZA);
+    showMovies(sortedMovies);
+    console.log(sortedMovies);
+  });
+  sortReleaseYearButtonOld.addEventListener('click', function () {
+    const sortedYear = sortReleaseYearButtonOld.value;
+    const sortedMovies = sortByReleaseYearOld(movies, sortedYear);
+    showMovies(sortedMovies);
+    console.log(sortedMovies);
+  });
+  sortReleaseYearButtonNew.addEventListener('click', function () {
+    const sortedYear = sortReleaseYearButtonNew.value;
+    const sortedMovies = sortByReleaseYearNew(movies, sortedYear);
+    showMovies(sortedMovies);
+    console.log(sortedMovies);
+  });
+  sortRottenTomatoesButtonHigh.addEventListener('click', function () {
+    const sortedRT = sortRottenTomatoesButtonHigh.value;
+    const sortedMovies = sortByRottenTomatoesHigh(movies, sortedRT);
+    showMovies(sortedMovies);
+    console.log(sortedMovies);
+  });
+  sortRottenTomatoesButtonLow.addEventListener('click', function () {
+    const sortedRT = sortRottenTomatoesButtonLow.value;
+    const sortedMovies = sortByRottenTomatoesLow(movies, sortedRT);
+    showMovies(sortedMovies);
+    console.log(sortedMovies);
+  });
+
 
   function showMovies(movies) {
     const moviesContainer = document.getElementById('cards-container');
-  moviesContainer.innerHTML = ''; // Limpa o conteúdo atual
+    moviesContainer.innerHTML = ''; // Limpa o conteúdo atual
 
-  movies.forEach(movie => {
-    const movieCard = createMovieCard(movie);
-    moviesContainer.appendChild(movieCard);
-  });
-}
+    movies.forEach(movie => {
+      const movieCard = createMovieCard(movie);
+      moviesContainer.appendChild(movieCard);
+    });
+  }
 
-function createMovieCard(movie) {
-  const card = document.createElement('div');
-  card.classList.add('movie-card');
+  function createMovieCard(movie) {
+    const card = document.createElement('div');
+    card.classList.add('movie-card');
 
-  // Frente do card
-  const front = document.createElement('div');
-  front.classList.add('card-front');
-  front.innerHTML = `
+    // Frente do card
+    const front = document.createElement('div');
+    front.classList.add('card-front');
+    front.innerHTML = `
     <img src="${movie.poster}" alt="${movie.title}" />
     <h3>${movie.title}</h3>
     <h4>Ano de Lançamento: ${movie.release_date}</h4>
   `;
 
-  // Verso do card
-  const back = document.createElement('div');
-  back.classList.add('card-back');
-  back.innerHTML = `
+    // Verso do card
+    const back = document.createElement('div');
+    back.classList.add('card-back');
+    back.innerHTML = `
     <h3>${movie.description}</h3>
     <h4>Diretor: ${movie.director}</h4>
     <h4>Nota no Rotten Tomatoes: ${movie.rt_score}</h4>
   `;
 
-  card.appendChild(front);
-  card.appendChild(back);
+    card.appendChild(front);
+    card.appendChild(back);
 
-  // Adicione qualquer lógica adicional para interatividade, como o efeito de flip do card
+    // Adicione qualquer lógica adicional para interatividade, como o efeito de flip do card
 
-  return card;
-}
+    return card;
+  }
 
 
   function showCharacter(characters) {
     const characterContainer = document.getElementById('cards-container');
-  characterContainer.innerHTML = ''; // Limpa o conteúdo atual
+    characterContainer.innerHTML = ''; // Limpa o conteúdo atual
 
-  characters.forEach(charac => {
-    const characterCard = createCharacterCard(charac);
-    characterContainer.appendChild(characterCard);
-  });
-}
+    characters.forEach(charac => {
+      const characterCard = createCharacterCard(charac);
+      characterContainer.appendChild(characterCard);
+    });
+  }
 
-function createCharacterCard(charac) {
-  const card = document.createElement('div');
-  card.classList.add('character-card');
+  function createCharacterCard(charac) {
+    const card = document.createElement('div');
+    card.classList.add('character-card');
 
-  // Frente do card
-  const front = document.createElement('div');
-  front.classList.add('card-front');
-  front.innerHTML = `
+    // Frente do card
+    const front = document.createElement('div');
+    front.classList.add('card-front');
+    front.innerHTML = `
     <img src="${charac.img}" alt="${charac.name}" />
     <h3>${charac.name}</h3>
     <h4>Idade: ${charac.age}</h4>
   `;
 
-  // Verso do card
-  const back = document.createElement('div');
-  back.classList.add('card-back');
-  back.innerHTML = `
+    // Verso do card
+    const back = document.createElement('div');
+    back.classList.add('card-back');
+    back.innerHTML = `
     <h4>Espécie: ${charac.specie}</h4>
     <h4>Gênero: ${charac.gender}</h4>
     <h4>Cor dos Olhos: ${charac.eye_color}</h4>
     <h4>Cor do Cabelo: ${charac.hair_color}</h4>
   `;
 
-  card.appendChild(front);
-  card.appendChild(back);
+    card.appendChild(front);
+    card.appendChild(back);
 
-  // Adicione qualquer lógica adicional para interatividade, como o efeito de flip do card
+    // Adicione qualquer lógica adicional para interatividade, como o efeito de flip do card
 
-  return card;
-}})
+    return card;
+  }
+})

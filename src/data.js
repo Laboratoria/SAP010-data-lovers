609071d8dc7d91f719c20c841915ad5e797dbdd9
Lila <lilaar91@gmail.com>
2023-06-06 14:45:58 -0300

    organizando o que sera feito no data.js

--- src/data.js
+++ src/data.js
@@ -1,12 +1,8 @@
 import data from './data/ghibli/ghibli.js';
 export const selectTitle = () => {
 
-  data.films[0].title
-
-  const titulos = [];
-
-  for (let i=0; i<data.films.length; i++) {
-    titulos.push(data.films[i].title);
-  }
-  console.log(titulos);
+  /* sort() -> alfabeto
+            -> lancamento
+            -> rating
+  */
 }

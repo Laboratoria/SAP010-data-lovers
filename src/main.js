898a344ffd744200741848ebdf7fe44b785c342c
Lila <lilaar91@gmail.com>
2023-06-06 14:59:28 -0300

    organizando DOM - 0.0 - o que será feito

--- src/main.js
+++ src/main.js
@@ -1,25 +1,48 @@
 //import { example } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
-import {selectTitle} from './data.js';
+import {} from './data.js';
 
+/* ToDo:
+      Organizar DOM
+            -> button onClick : pega o input do elemento HTML
+            input list="first-filter" -> datalist id="first-filter" -> option value
+
+            If (<nao-sei-como-referenciar>.innerHTML == "Títulos de a-z")
+            {
+              funcao sort por ordem alfabetica.
+            }
+            else if (<nao-sei-como-referenciar>.innerHTML == "Ano de lancamento")
+            {
+              funcao sort por ano lancamento *precisa transformar string p number
+            }
+            else if (<nao-sei-como-referenciar>.inner == "Rating")
+            {
+              funcao sort por rating -> transformar string pra double.
+            }
+            else
+            {
+              volta todos os cards.
+            }
+
+            **TODAS AS FUNCOES SAO DEFINIDAS NO DATA.JS
+
+            ***Chamo os cards no main ou no data.js?
+*/
 
 console.log(data);
 //document.getElementById("start-events").addEventListener("click", );
-console.log(selectTitle);
+
 
 console.log(data.films[0].title);
-
-//console.log(data.films.title.sort());
+//devolve um título.
 
 const titulos = [];
-
 for (let i=0; i<data.films.length; i++) {
   const card = `<h1>${data.films[i].title}</h1>`;
   titulos.push(card);
 //fazer o sort na array de filmes
 }
-//p sort funcionar c rating tem q transformar p int
 
 console.log(titulos);
 console.log(titulos.sort());
@@ -31,37 +54,6 @@ for (let i=0; i<data.films.length; i++){
 }
 console.log(rating);
 console.log(rating.sort());
-
-
-
-
-
-// document.getElementById("start-events").addEventListener("click", myFilters());
-
-// function myFilters() {
-//   const filterA = ["Ordem alfabetica", "Ano de lancamento", ""];
-//   const filterB = ["Diretor", "Produtor",""];
-//   const firstFilter = document.getElementById("first-filter").innerHTML;
-//   const secondFilter = document.getElementById("second-filter").innerHTML;
-
-//encontrar o valor firstFilter dentro do array filterA
-//encontrar o valor secondFilter dentro do array filterB
-//devolver os index.
-//executar as combinacoes
-//combinacoes sao?
-//"" e "" => devolve todos os filmes
-//"" e Diretor =>
-//"" e Produtor =>
-//Ordem alfabetica e "" =>
-//Ordem cronologica e "" =>
-//Ordem alfabetica e Diretor
-//Ordem alfabetica e Produtor
-//Ordem cronologica e Diretor
-//Ordem cronologica e Produtor
-//COMO FAZER ISSO USANDO OBJETO?
-//TALVEZ TENTANDO TRANSFORMAR O SWITCH EM OBJ LA NO CARD VALIDATOR EU APRENDA.
-
-//
 
 
 

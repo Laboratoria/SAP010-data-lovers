0a207431acb088a6be3f4b8927b239bdd8c1f70c
Lila <lilaar91@gmail.com>
2023-06-20 15:06:36 -0300

    atualizacoes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,29 @@
+<<<<<<< HEAD
 
 export function sortAlphabet(){
   //const titulos = [];
   //sort(escolha)
 }
 
+=======
+//import data from './data/ghibli/ghibli.js';
+
+export const searchTitle = (titles, name) => {
+  const filterTitle = title => title.name.toUpperCase().includes(name.toUpperCase());
+  const filter = titles.filter(filterTitle);
+  return filter;
+}
+
+export function sortAlphabet(){
+  const orderByAlphabet = [];
+  orderByAlphabet.sort((a, b) => {
+    if (a.title.toUpperCase < b.title.toUpperCase){
+      return -1;
+    }
+  })
+}
+
+>>>>>>> b0b5421 (atualizacoes html css js)
 export function sortRealease(){
   //string p number
   //sort
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //import { example } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
+<<<<<<< HEAD
 import { sortAlphabet } from './data.js';
 
 const selectElement = document.querySelector("#first-filter");
@@ -10,36 +11,68 @@ console.log(selectElement);
 /* ToDo:
       Organizar DOM
       onChange ->
+=======
+import {} from './data.js';
+import { title } from 'process';
 
-            **TODAS AS FUNCOES SAO DEFINIDAS NO DATA.JS
+const titles = data.films(title);
 
+document.getElementById("start-event").addEventListener("click", myFunction);
+>>>>>>> b0b5421 (atualizacoes html css js)
+
+
+<<<<<<< HEAD
             ***Chamo os cards no main ou no data.js? No main mesmo? Sim.
 */
 
 //console.log(data);
+=======
+function myFunction(){
+  const element = document.getElementById("first-filter");
+  const valorSel = element.options[element.selectedIndex].value;
+  const textoSel = element.options[element.selectedIndex].text;
+  console.log(valorSel);
+  console.log(textoSel);
+
+  if (valorSel === "title-az"){
+    //document.createElement("section").innerHTML;
+
+    }
+    document.getElementById("card-container").innerHTML = titulos;
+  }
+  else if (valorSel === "lancamento"){
+    const lancamento = [];
+    for (let i = 0; i < data.films.length; i++){
+      lancamento.push(data.films[i].release_date);
+    }
+    console.log(lancamento);
+  }
+  else if (valorSel === "rating"){
+    const rating = [];
+    for (let i=0; i<data.films.length; i++){
+      rating.push(data.films[i].rt_score);
+    }
+    console.log(rating.sort());
+  }
+  else {°
+    const titulos = [];
+    for (let i=0; i<data.films.length; i++) {
+      const card = `<h1>${data.films[i].title}</h1>`;
+      titulos.push(card);
+  }
+}
+
+
+>>>>>>> b0b5421 (atualizacoes html css js)
 //document.getElementById("start-events").addEventListener("click", );
 
 
-console.log(data.films[0].title);
+//console.log(data.films[0].title);
 //devolve um título.
 
-const titulos = [];
-for (let i=0; i<data.films.length; i++) {
-  const card = `<h1>${data.films[i].title}</h1>`;
-  titulos.push(card);
-//fazer o sort na array de filmes
-}
-
-console.log(titulos);
-console.log(titulos.sort());
 
 
-const rating = [];
-for (let i=0; i<data.films.length; i++){
-  rating.push(data.films[i].rt_score);
-}
-console.log(rating);
-console.log(rating.sort());
+
 
 
 

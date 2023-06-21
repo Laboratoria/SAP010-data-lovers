f2be031a98c40c4b7bc8e8de49e8cf3ab54b3013
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2023-06-21 18:45:14 -0300

    Adicionamos o filter para selecionar por tipo de pokemon

--- src/data.js
+++ src/data.js
@@ -1,15 +1,18 @@
 
 export function pokeType(pokeArray, pokeChange) {
+    if (pokeChange === "allPoke") {
+        return pokeArray
+    }
     const typeFiltered = pokeArray.filter(pokemon => pokemon.type.includes(pokeChange))
     return typeFiltered;
-}
+};
 
-export function pokeOrder( pokeChangeOrder) {
+/*export function pokeOrder( pokeChangeOrder) {
     const ordered = pokeChangeOrder.sort((a, z) => {
         if (a > z) return 1
         if (a < z) return -1
         return 0
     }
     )
-}
+}*/
 

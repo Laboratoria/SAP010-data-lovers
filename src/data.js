55e1a944d0252aaa99a158666c352fb074bad716
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2023-06-22 12:22:06 -0300

    Atualização final do filter

--- src/data.js
+++ src/data.js
@@ -7,12 +7,12 @@ export function pokeType(pokeArray, pokeChange) {
     return typeFiltered;
 };
 
-/*export function pokeOrder( pokeChangeOrder) {
-    const ordered = pokeChangeOrder.sort((a, z) => {
+/*export function pokeOrder(pokeChangeOrder) {
+    const ordered = pokeChangeOrder.sort((a,z) => {
         if (a > z) return 1
         if (a < z) return -1
         return 0
     }
-    )
-}*/
+)}*/
+
 

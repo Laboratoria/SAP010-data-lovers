917c3cfa5a896f760658f479dcfd03f57c90c649
Lila <lilaar91@gmail.com>
2023-06-26 10:59:08 -0300

    data.js - ordenando objetos atraves de atributos

--- src/data.js
+++ src/data.js
@@ -1,48 +1,79 @@
 
-//import data from './data/ghibli/ghibli.js';
+import data from './data/ghibli/ghibli.js';
 
-export const searchTitle = (titles, name) => {
-  const filterTitle = title => title.name.toUpperCase().includes(name.toUpperCase());
-  const filter = titles.filter(filterTitle);
-  return filter;
-}
-
-export function sortAlphabet(){
+const dataGhibli = data.films;
+//nao preciso acessar o titulo nessa constante, ele sera acessado na funcao!!
+export function sortAlphabet(dataGhibli){
   const orderByAlphabet = [];
-  if (valorSel === "title-az"){
-    orderByAlphabet.sort((a, b) => {
-      if (a.title.toUpperCase < b.title.toUpperCase){
-        return -1;
-      }
-    })
-  }
+  orderByAlphabet.sort((a, b) => {
+    if (a.title.toUpperCase < b.title.toUpperCase){
+      return -1;
+    }
+  })
   return orderByAlphabet;
 }
 
-export function sortRealease(){
+export function sortRealease(dataGhibli){
   const orderByRealease = [];
-  if (valorSel === "lancamento"){
-    orderByRealease.sort((a, b) => {
-      if (a.release_date < b.release_date) {
-        return -1;
-      }
-    })
-  }
+  orderByRealease.sort((a, b) => {
+    if (a.release_date < b.release_date) {
+      return -1;
+    }
+  })
   return orderByRealease;
 }
 
-export function sortRating(){
+export function sortRating(dataGhibli){
   const orderByRating = [];
-  if (valorSel === "rating"){
-    orderByRating.sort((a, b) => {
-      if (a.rt_score < b.rt_score){
-        return -1;
-      }
-    })
-  }
+  orderByRating.sort((a, b) => {
+    if (a.rt_score < b.rt_score){
+      return -1;
+    }
+  })
   return orderByRating;
 }
 export function filterDirector(){
   //
 }
 
+
+// const order = {
+//   byAlphabet: function() {
+//     const orderByAlphabet = [];
+//     orderByAlphabet.sort((a, b) => {
+//       if (a.title.toUpperCase < b.title.toUpperCase){
+//         return -1;
+//       }
+//     })
+//     return orderByAlphabet;
+//   },
+
+//   byRealease: function() {
+//     const orderByRealease = [];
+//     orderByRealease.sort((a, b) =>
+//       a.release_date - b.release_date
+//     )
+//     return orderByRealease;
+//   },
+
+//   byRating: function() {
+//     <>.sort((a, b) => a.title - b.title);
+//   }
+
+
+// //const dataFilms = data.films.title;
+
+
+// export const searchTitle = (titles, name) => {
+//   const filterTitle = title => title.name.toUpperCase().includes(name.toUpperCase());
+//   const filter = titles.filter(filterTitle);
+//   return filter;
+//   return filter;
+// }
+
+
+{/* export function orderByNumber(arr){
+  arr.sort((a, b) => a.title - b.title);
+}
+orderByAZ(data); */}
+}
--- src/main.js
+++ src/main.js
@@ -1,38 +1,32 @@
 //import { example } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
-import {} from './data.js';
-import { title } from 'process';
-import {sortAlphabet} from './data.js';
+import {sortAlphabet, sortRating, sortRealease} from './data.js';
 
-const titles = data.films(title);
-const textoSel = element.options[element.selectedIndex].text;
+// const titles = data.films(title);
+// const textoSel = element.options[element.selectedIndex].text;
 
 document.getElementById("start-event").addEventListener("click", myFunction);
+document.getElementById("first-filter").addEventListener("change", myFunction);
 
 
 function myFunction(){
   const element = document.getElementById("first-filter");
   const valorSel = element.options[element.selectedIndex].value;
-  const textoSel = element.options[element.selectedIndex].text;
-  console.log(valorSel);
-  console.log(textoSel);
 
   if (valorSel === "title-az"){
-    sortAlphabet(valorSel);
-
-    //document.createElement("section").innerHTML;
-    document.getElementById("card-container").innerHTML = titulos;
+    
+    document.getElementById("card-container").innerHTML = titleAZ;
   }
   else if (valorSel === "lancamento"){
-    const lancamento = [];
+    const lancamento = sortRealease(data.films.release_date);
     for (let i = 0; i < data.films.length; i++){
       lancamento.push(data.films[i].release_date);
     }
     console.log(lancamento);
   }
   else if (valorSel === "rating"){
-    const rating = [];
+    const rating = sortRating(data.films.rating);
     for (let i=0; i<data.films.length; i++){
       rating.push(data.films[i].rt_score);
     }
@@ -47,5 +41,8 @@ function myFunction(){
   }
 }
 
-
 //document.getElementById("start-events").addEventListener("click", );
+
+
+//console.log(data.films[0].title);
+//devolve um título.

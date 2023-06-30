25213cf0aa68c8f591e6dd90d023dc35a292d071
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-06-30 14:42:51 -0300

    inserindo códigos dentro da função paises

--- src/main.js
+++ src/main.js
@@ -5,25 +5,29 @@ import data from './data/countries/countries.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(data.countries[0], data.countries[0].population);
+function paises() {
 
- for(let i = 0; i < data.countries.length; i++){
+    for (let i = 0; i < data.countries.length; i++) {
 
-    const exibirFlags  = data.countries[i].flags.png
-    const exibirName = data.countries[i].name.common
-    const exibirPopulation = data.countries[i].population
-    
-    // template string
-    const template = `
+        const exibirFlags = data.countries[i].flags.png
+        const exibirName = data.countries[i].name.common
+        const exibirPopulation = data.countries[i].population
+
+        // template string
+        const template = `
     <img src ="${exibirFlags}"> 
     <h3>${exibirName}</h3> 
     <p> População ${exibirPopulation}</p> `
 
-const exibirFlagsDiv = document.getElementById("exibirFlags");
-exibirFlagsDiv.innerHTML= template;
+        const exibirFlagsDiv = document.getElementById("exibirFlags");
+        exibirFlagsDiv.innerHTML = exibirFlagsDiv.innerHTML + template;
 
 
-    
 
-    console.log(template) //como inserir template no dom com inner html
-    
- }
+
+        console.log(template)
+
+    }
+}
+
+paises();

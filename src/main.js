e8a2e985827fad44456a089d117b92c05dd236b6
cilafreitas <cila123xavier@gmail.com>
2023-06-30 09:39:48 -0300

    imprimimos todos os cards na tela

--- src/main.js
+++ src/main.js
@@ -1,29 +1,24 @@
 //import { example } from './data.js';
-import countries from './data/countries/countries.js';
+import countries from "./data/countries/countries.js";
 // import data from './data/lol/lol.js';
-import data from './data/countries/countries.js';
+import data from "./data/countries/countries.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(data.countries[0], data.countries[0].population);
 
- for(let i = 0; i < data.countries.length; i++){
+for (let i = 0; i < data.countries.length; i++) {
+  const exibirFlags = data.countries[i].flags.png;
+  const exibirName = data.countries[i].name.common;
+  const exibirPopulation = data.countries[i].population;
 
-    const exibirFlags  = data.countries[i].flags.png
-    const exibirName = data.countries[i].name.common
-    const exibirPopulation = data.countries[i].population
-    
-    // template string
-    const template = `
+  // template string
+  const template = `
     <img src ="${exibirFlags}"> 
     <h3>${exibirName}</h3> 
-    <p> População ${exibirPopulation}</p> `
+    <p> População ${exibirPopulation}</p> `;
 
-const exibirFlagsDiv = document.getElementById("exibirFlags");
-exibirFlagsDiv.innerHTML= template;
+  const exibirFlagsDiv = document.getElementById("exibirFlags");
+  exibirFlagsDiv.innerHTML = exibirFlagsDiv.innerHTML + template
 
-
-    
-
-    console.log(template) //como inserir template no dom com inner html
-    
- }
+  console.log(template); //como inserir template no dom com inner html
+}

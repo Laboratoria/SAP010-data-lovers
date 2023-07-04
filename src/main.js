2d4d23c1668e580e555e8728f7d9e02ce585750e
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-07-04 14:58:03 -0300

    criando paramentro no funcao

--- src/main.js
+++ src/main.js
@@ -1,33 +1,34 @@
 //import { example } from './data.js';
-import countries from './data/countries/countries.js';
+import countries from "./data/countries/countries.js";
 // import data from './data/lol/lol.js';
-import data from './data/countries/countries.js';
+import data from "./data/countries/countries.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(data.countries[0], data.countries[0].population);
 function paises(array) {
 
-    for (let i = 0; i < array.length; i++) {
+ for(let i = 0; i < data.countries.length; i++){
 
-        const exibirFlags = array[i].flags.png
-        const exibirName = array[i].name.common
-        const exibirPopulation = array[i].population
-
-        // template string
-        const template = `
+    const exibirFlags  = data.countries[i].flags.png
+    const exibirName = data.countries[i].name.common
+    const exibirPopulation = data.countries[i].population
+    
+    // template string
+    const template = `
     <img src ="${exibirFlags}"> 
     <h3>${exibirName}</h3> 
-    <p> População ${exibirPopulation}</p> `
+    <p> População ${exibirPopulation}</p> `;
 
-        const exibirFlagsDiv = document.getElementById("exibirFlags");
-        exibirFlagsDiv.innerHTML = exibirFlagsDiv.innerHTML + template;
-
-
+const exibirFlagsDiv = document.getElementById("exibirFlags");
+exibirFlagsDiv.innerHTML= template;
 
 
-        console.log(template)
+    
 
-    }
+    console.log(template) //como inserir template no dom com inner html
+    
+ }
+
 }
 
 paises(data.countries);

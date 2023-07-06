9daa1a81ff4f026328ca5228938fd8d6f3385e72
Lila <lilaar91@gmail.com>
2023-07-05 22:29:16 -0300

    mudancas no DOM e no CSS

--- src/main.js
+++ src/main.js
@@ -7,9 +7,46 @@ const dataGhibli = data.films;
 
 // const titles = data.films(title);
 // const textoSel = element.options[element.selectedIndex].text;
+document.getElementById("totoro-logo").addEventListener("mouseover", changeColor);
+function changeColor(){
+  console.log("changeColor event");
+  const changeColor = document.getElementById("totoro-logo");
+  changeColor.classList.add("invert-color");
+}
+
+document.getElementById("totoro-logo").addEventListener("mouseout", originalColor());
+function originalColor(){
+  const originalColor = document.getElementById("totoro-logo");
+  originalColor.style.backgroundColor = "hex(#7FFFD4)";
+}
+
+document.getElementById("totoro-logo").addEventListener("click", createMenu);
+function createMenu(){
+  console.log("entrou");
+  const whereInit = document.getElementById("create-menu");
+  const menuInicial = document.createElement('ul');
+
+  menuInicial.innerHTML = '<li class="item" name="e"><a href="https://studioghibli.com.br/ghiblistore/livros/">BOOKS</a></li> <li class="item" name="e"><a href="https://studioghibli.com.br/categoria/games/">GAMES</a></li>';
+  whereInit.appendChild(menuInicial);
+}
+
+
+document.getElementById("cabecalho").addEventListener("mouseout", hideCreatedMenu)
+function hideCreatedMenu(){
+  //console.log("testando hideCreatedMenu");
+  const where = document.getElementById("create-menu");
+  const menuInicial = document.getElementsByClassName("item");
+  if (menuInicial){
+    where.removeChild(menuInicial);
+  }
+}
+
+document.getElementById("img-footer").addEventListener("mouseover", createSocialMediaButtons);
+function createSocialMediaButtons(){
+  console.log("socialmedia buttons");
+}
 
 document.getElementById("first-filter").addEventListener("change", myFunction);
-
 function myFunction(){
   const element = document.getElementById("first-filter");
   const valorSel = element.options[element.selectedIndex].value;
@@ -75,4 +112,3 @@ function chooseDirector(){
   const returnCardsDirector = directorArray.join(" ");
   document.getElementById("card-container").innerHTML = returnCardsDirector;
 }
-

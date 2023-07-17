6320116686f3520143a5b40b6636368779638800
cilafreitas <cila123xavier@gmail.com>
2023-07-17 18:21:20 -0300

    testando filtro continente

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,9 @@
-import { example, anotherExample } from '../src/data.js';
+import { filtrarPorContinente } from "../src/data.js";
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe("testando filtro continente", () => {
+  it("deve retornar os continentes", () => {
+    expect(filtrarPorContinente()).toBe(expected);
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});

bf0a5d11236da829cb1bfbb59970584835c5e4e5
cilafreitas <cila123xavier@gmail.com>
2023-07-17 22:03:08 -0300

    inserindo teste para função continente

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,7 +1,42 @@
 import { filtrarPorContinente } from "../src/data.js";
 
+const filtroContinentes = [
+  { 
+    "continents": [
+      "America"
+  ]
+},
+{
+  "continents": [
+    "Asia"
+  ]
+},
+{
+  "continents": [
+    "Europe"
+  ]
+}
+],
+
 describe("testando filtro continente", () => {
-  it("deve retornar os continentes", () => {
-    expect(filtrarPorContinente()).toBe(expected);
+  const expected = [
+    { 
+      "continents": [
+        "America"
+    ]
+  },
+  {
+    "continents": [
+      "Asia"
+    ]
+  },
+  {
+    "continents": [
+      "Europe"
+    ]
+  }
+  ],
+  it("deve retornar os países de cada continente", () => {
+    expect(filtrarPorContinente(filtroContinentes, continente)).toStrictEqual(expected);
   });
 });
